d7d6a625af2d411066642048b9ff9f48df559c33
Nathan Baines <supernath126@gmail.com>
2025-03-11 14:45:59 +0000

    Initial commit

---
+++ header.php
@@ -0,0 +1,22 @@
+<?php $__APP_NAME__ = ""; include("start-session.php"); ?>
+
+<div class="navbar column align-center justify-center">
+    <div style="width: 100%" class="row align-center justify-center">
+        <div class="column align-center justify-center">
+            <h1 id="navbar-title"><?php echo $__APP_NAME__; ?></h1>
+            <a href="https://apps.nathcat.net">Part of the Nathcat Network</a>
+        </div>
+
+        <?php if (!isset($__REMOVE_PROFILE_BANNER__)) : ?>
+        <span class="half-spacer"></span>
+
+        <div style="cursor: pointer;" class="row justify-center align-center" onclick="location = '/user';">
+            <div class="small-profile-picture">
+                <img src="<?php echo "$_DATA_BASE_URL/pfps/" . $_SESSION["user"]["pfpPath"]; ?>">
+            </div>
+
+            <h2 style="margin-left: 20px;"><?php echo $_SESSION["user"]["fullName"]; ?></h2>
+        </div>
+        <?php endif; ?>
+    </div>
+</div>
---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title><?php echo $__APP_NAME__; ?></title>
+
+        <link rel="stylesheet" href="https://nathcat.net/static/css/new-common.css">
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Montserrat&display=swap" rel="stylesheet">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
+    </head>
+
+    <body>
+        <div id="page-content" class="content">
+            <?php include("header.php"); ?>
+
+            <?php include("footer.php"); ?>
+        </div>
+    </body>
+</html>
